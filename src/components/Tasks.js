5f8c7210d134acd5ad89858cd3b242c99e1cfda6
liberatrice bayizere <92096502+liberatricebaizer@users.noreply.github.com>
2023-02-14 10:19:10 +0200

    new things

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Checkbox } from "./Checkbox";
+import { useTasks } from "../hooks";
 
 export const Tasks = () => {
-  const tasks = [];
+  const { tasks } = useTasks("1");
   let projectName = "";
+  console.log(tasks);
 
   return (
     <div className="tasks" data-testid="tasks">
       <h2 data-testid="project-name">{projectName}</h2>
+      <ul className="tasks__list">
+        {tasks.map((task) => (
+          <li key={`${task.id}`}>
+            <Checkbox id={task.id} />
+            <span>{task.task}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
