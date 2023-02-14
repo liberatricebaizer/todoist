25642a3781347882405ee894f424b3fb4cb7bf60
liberatrice bayizere <92096502+liberatricebaizer@users.noreply.github.com>
2023-02-14 11:40:49 +0200

    new things

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { App } from "./App";
+import "./App.scss";
 
 // import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 render(<App />, document.getElementById("root"));
